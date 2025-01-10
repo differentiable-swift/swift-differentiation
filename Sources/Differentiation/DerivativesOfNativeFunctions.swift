67648952b72a6e0f8e027bd35fb47dc80294f3fd
Jaap Wijnen <wijnen.jaap@gmail.com>
2025-01-10 17:08:14 +0100

    refactor: make the vjp's for min max abs more consistent
    vjp's are now more consistent with their original implementation and link to their original source.

--- Sources/Differentiation/DerivativesOfNativeFunctions.swift
+++ Sources/Differentiation/DerivativesOfNativeFunctions.swift
@@ -6,55 +6,30 @@ import _Differentiation
 /// https://github.com/apple/swift/blob/main/stdlib/public/core/Algorithm.swift#L18
 @inlinable
 @derivative(of: min(_:_:))
-public func _vjpMin<T: Comparable & Differentiable>(
-    _ lhs: T,
-    _ rhs: T
-) -> (value: T, pullback: (T.TangentVector) -> (T.TangentVector, T.TangentVector)) {
-    func pullback(_ tangentVector: T.TangentVector) -> (T.TangentVector, T.TangentVector) {
-        if lhs <= rhs {
-            return (tangentVector, .zero)
-        }
-        else {
-            return (.zero, tangentVector)
-        }
-    }
-    return (value: min(lhs, rhs), pullback: pullback)
+public func _vjpMin<T: Comparable & Differentiable>(_ x: T, _ y: T)
+    -> (value: T, pullback: (T.TangentVector) -> (T.TangentVector, T.TangentVector))
+{
+    y < x ? (value: y, pullback: { v in (.zero, v) }) : (value: x, pullback: { v in (v, .zero) })
 }
 
 /// For max(): "Returns: The greater of `x` and `y`. If `x` is equal to `y`, returns `y`."
 /// https://github.com/apple/swift/blob/main/stdlib/public/core/Algorithm.swift#L52
 @inlinable
 @derivative(of: max(_:_:))
-public func _vjpMax<T: Comparable & Differentiable>(
-    _ lhs: T,
-    _ rhs: T
-) -> (value: T, pullback: (T.TangentVector) -> (T.TangentVector, T.TangentVector)) {
-    func pullback(_ tangentVector: T.TangentVector) -> (T.TangentVector, T.TangentVector) {
-        if lhs < rhs {
-            return (.zero, tangentVector)
-        }
-        else {
-            return (tangentVector, .zero)
-        }
-    }
-    return (value: max(lhs, rhs), pullback: pullback)
+public func _vjpMax<T: Comparable & Differentiable>(_ x: T, _ y: T)
+    -> (value: T, pullback: (T.TangentVector) -> (T.TangentVector, T.TangentVector))
+{
+    y >= x ? (value: y, pullback: { v in (.zero, v) }) : (value: x, pullback: { v in (v, .zero) })
 }
 
 /// To differentiate ``abs``
+/// https://github.com/swiftlang/swift/blob/main/stdlib/public/core/Integers.swift#L346
 @inlinable
 @derivative(of: abs(_:))
-public func _vjpAbs<T: Comparable & SignedNumeric & Differentiable>(_ value: T)
+public func _vjpAbs<T: SignedNumeric & Comparable & Differentiable>(_ x: T)
     -> (value: T, pullback: (T.TangentVector) -> T.TangentVector)
 {
-    func pullback(_ tangentVector: T.TangentVector) -> T.TangentVector {
-        if value < 0 {
-            return .zero - tangentVector
-        }
-        else {
-            return tangentVector
-        }
-    }
-    return (value: abs(value), pullback: pullback)
+    x < 0 ? (value: -x, pullback: { v in .zero - v }) : (value: x, pullback: { v in v })
 }
 
 #endif
