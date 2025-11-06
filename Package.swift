c44f92b43f9428ad11af5f86ce0a1db44871f72a
Jaap Wijnen <wijnen.jaap@gmail.com>
2025-11-06 10:16:10 -0700

    refactor: move benchmarks to subpackage to not add dependency to main lib

--- Package.swift
+++ Package.swift
@@ -15,34 +15,11 @@ let package = Package(
             targets: ["Differentiation"]
         ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.4"),
-    ],
     targets: [
         .target(name: "Differentiation"),
         .testTarget(
             name: "DifferentiationTests",
             dependencies: ["Differentiation"]
-        ),
-        .executableTarget(
-            name: "Benchmarks",
-            dependencies: [
-                "Differentiation",
-                .product(name: "CollectionsBenchmark", package: "swift-collections-benchmark"),
-            ],
-            swiftSettings: [
-//                .unsafeFlags([
-//                    "-Rpass-missed=specialize",
-//                    "-O",
-//                    "-g",
-//                ])
-            ]
-        ),
-        .testTarget(
-            name: "BenchmarkTests",
-            dependencies: [
-                "Benchmarks",
-            ]
         ),
     ]
 )
