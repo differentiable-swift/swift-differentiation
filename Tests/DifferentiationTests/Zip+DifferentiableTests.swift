a3b96e3d93afb399e25ddb5afc5fa08385de9686
Jaap Wijnen <wijnen.jaap@gmail.com>
2025-12-08 15:13:50 +0100

    feat: add arity3 zip

--- Tests/DifferentiationTests/Zip+DifferentiableTests.swift
+++ Tests/DifferentiationTests/Zip+DifferentiableTests.swift
@@ -61,13 +61,81 @@ struct ZipDifferentiableTests {
         let a: [Double] = [1, 2, 3]
         let b: [Double] = [5, 6, 7]
 
-        let (value, pullback) = valueWithPullback(at: a, b, of: { s1, s2 in
+        let expectedValue = 24.0
+        let expectedGradient: ([Double].TangentVector, [Double].TangentVector) = ([1.0, 1.0, 1.0], [1.0, 1.0, 1.0])
+
+        let (zipValue, zipPullback) = valueWithPullback(at: a, b, of: { s1, s2 in
             differentiableZip(s1, s2).differentiableMap { $0 + $1 }.differentiableReduce(0.0) { $0 + $1 }
         })
 
-        #expect(value == 24.0)
+        #expect(zipValue == expectedValue)
 
-        let gradient = pullback(1.0)
-        #expect(gradient == ([1.0, 1.0, 1.0], [1.0, 1.0, 1.0]))
+        let gradient = zipPullback(1.0)
+        #expect(gradient.0 == expectedGradient.0)
+
+        let (value1, pullback1) = valueWithPullback(at: a, b, of: { s1, s2 in
+            var result = 0.0
+            for i in 0 ..< withoutDerivative(at: s1.count) {
+                result += s1[i] + s2[i]
+            }
+
+            return result
+        })
+
+        #expect(value1 == expectedValue)
+
+        let gradient1 = pullback1(1.0)
+        #expect(gradient1 == expectedGradient)
     }
+    
+    @Test
+    func arity3Zip() {
+        let a: [Double] = [1, 2, 3]
+        let b: [Double] = [4, 5, 6]
+        let c: [Double] = [7, 8, 9]
+        
+        let expectedValue = 45.0
+        let expectedGradient: ([Double].TangentVector, [Double].TangentVector, [Double].TangentVector) = ([1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0])
+
+        let (zipValue, zipPullback) = valueWithPullback(at: a, b, c, of: { (s1: [Double], s2: [Double], s3: [Double]) in
+            differentiableZip(s1, s2, s3).differentiableMap { $0 + $1 + $2 }.differentiableReduce(0.0) { $0 + $1 }
+        })
+
+        #expect(zipValue == expectedValue)
+
+        let gradient = zipPullback(1.0)
+        #expect(gradient.0 == expectedGradient.0)
+
+        let (value1, pullback1) = valueWithPullback(at: a, b, c, of: { s1, s2, s3 in
+            var result = 0.0
+            for i in 0 ..< withoutDerivative(at: s1.count) {
+                result += s1[i] + s2[i] + s3[i]
+            }
+
+            return result
+        })
+
+        #expect(value1 == expectedValue)
+
+        let gradient1 = pullback1(1.0)
+        #expect(gradient1 == expectedGradient)
+    }
+
+// MARK: Currently not supported.
+//    @Test
+//    func nestedZip() {
+//        let a: [Double] = [1, 2, 3]
+//        let b: [Double] = [5, 6, 7]
+//        let c: [Double] = [9, 8, 7]
+//
+//        let (value, pullback) = valueWithPullback(at: a, b, c, of: { s1, s2, s3 in
+//            differentiableZip(
+//                s1,
+//                differentiableZip(
+//                    s2,
+//                    s3
+//                )
+//            ).differentiableMap { pair in pair.a + pair.b.a + pair.b.b }
+//        })
+//    }
 }
