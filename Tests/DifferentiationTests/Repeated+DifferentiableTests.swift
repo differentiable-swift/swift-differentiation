e7942590e27566871e014099f0691d4681fd14a8
Jaap Wijnen <wijnen.jaap@gmail.com>
2025-12-08 14:26:47 +0100

    feat: make Repeated subscript differentiable

--- Tests/DifferentiationTests/Repeated+DifferentiableTests.swift
+++ Tests/DifferentiationTests/Repeated+DifferentiableTests.swift
@@ -33,19 +33,41 @@ struct RepeatedDifferentiableTests {
         let a: [Double] = [1, 2, 3]
         let b: Repeated<Double> = repeatElement(2.0, count: 3)
 
-        let (value, pullback) = valueWithPullback(at: a, b, of: { s1, s2 in
+        let expectedValue = [2.0, 4.0, 6.0]
+
+        let (value0, pullback0) = valueWithPullback(at: a, b, of: { s1, s2 in
             differentiableZip(s1, s2).differentiableMap { e1, e2 in e1 * e2 }
         })
 
-        #expect(value == [2.0, 4.0, 6.0])
-        let gradient1 = pullback([1.0, 0.0, 0.0])
-        #expect(gradient1.0 == [2.0, 0.0, 0.0])
-        #expect(gradient1.1.base.repeatedValue == 1.0)
-        #expect(gradient1.1.count == 3)
+        #expect(value0 == expectedValue)
+        let gradient01 = pullback0([1.0, 0.0, 0.0])
+        #expect(gradient01.0 == [2.0, 0.0, 0.0])
+        #expect(gradient01.1.base.repeatedValue == 1.0)
+        #expect(gradient01.1.count == 3)
 
-        let gradient2 = pullback([0.0, 1.0, 0.0])
-        #expect(gradient2.0 == [0.0, 2.0, 0.0])
-        #expect(gradient2.1.base.repeatedValue == 2.0)
-        #expect(gradient2.1.count == 3)
+        let gradient02 = pullback0([0.0, 1.0, 0.0])
+        #expect(gradient02.0 == [0.0, 2.0, 0.0])
+        #expect(gradient02.1.base.repeatedValue == 2.0)
+        #expect(gradient02.1.count == 3)
+
+        let (value1, pullback1) = valueWithPullback(at: a, b, of: { s1, s2 in
+            var results: [Double] = []
+            for i in 0 ..< withoutDerivative(at: s1.count) {
+                let value = s1[i] * s2[i]
+                results.append(value)
+            }
+            return results
+        })
+
+        #expect(value1 == expectedValue)
+        let gradient11 = pullback1([1.0, 0.0, 0.0])
+        #expect(gradient11.0 == [2.0, 0.0, 0.0])
+        #expect(gradient11.1.base.repeatedValue == 1.0)
+        #expect(gradient11.1.count == 3)
+
+        let gradient12 = pullback1([0.0, 1.0, 0.0])
+        #expect(gradient12.0 == [0.0, 2.0, 0.0])
+        #expect(gradient12.1.base.repeatedValue == 2.0)
+        #expect(gradient12.1.count == 3)
     }
 }
