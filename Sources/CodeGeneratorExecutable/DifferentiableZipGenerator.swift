7d025522494d5bc40c39d2d802a01050b99165ff
Jaap Wijnen <wijnen.jaap@gmail.com>
2025-12-12 15:25:37 +0100

    feat: add arity 13, 14

--- Sources/CodeGeneratorExecutable/DifferentiableZipGenerator.swift
+++ Sources/CodeGeneratorExecutable/DifferentiableZipGenerator.swift
@@ -200,7 +200,46 @@ enum ZipSequenceGenerator {
             )
         }
         
+        @usableFromInline
+        func valueWithPullback<M, N, O, P, Q, S, T, U, V, W, X, Y, Z, R>(
+          at m: M, _ n: N, _ o: O, _ p: P, _ q: Q, _ s: S, _ t: T, _ u: U, _ v: V, _ w: W, _ x: X, _ y: Y, _ z: Z, of f: @differentiable(reverse) (M, N, O, P, Q, S, T, U, V, W, X, Y, Z) -> R
+        ) -> (value: R,
+              pullback: (R.TangentVector)
+                -> (M.TangentVector, N.TangentVector, O.TangentVector, P.TangentVector, Q.TangentVector, S.TangentVector, T.TangentVector, U.TangentVector, V.TangentVector, W.TangentVector, X.TangentVector, Y.TangentVector, Z.TangentVector)) {
 
+            let (value, pullback) = valueWithPullback(at: Pair(Pair(Pair(m, n), o), Pair(p, q)), Pair(Pair(s, t), Pair(u, v)), Pair(Pair(w, x), Pair(y, z))) { pair1, pair2, pair3 in 
+                f(pair1.a.a.a, pair1.a.a.b, pair1.a.b, pair1.b.a, pair1.b.b, pair2.a.a, pair2.a.b, pair2.b.a, pair2.b.b, pair3.a.a, pair3.a.b, pair3.b.a, pair3.b.b)
+            }
+
+            return (
+                value: value,
+                pullback: { v in 
+                    let results = pullback(v)
+                    return (results.0.a.a.a, results.0.a.a.b, results.0.a.b, results.0.b.a, results.0.b.b, results.1.a.a, results.1.a.b, results.1.b.a, results.1.b.b, results.2.a.a, results.2.a.b, results.2.b.a, results.2.b.b)
+                }
+            )
+        }
+        
+        @usableFromInline
+        func valueWithPullback<L, M, N, O, P, Q, S, T, U, V, W, X, Y, Z, R>(
+          at l: L, _ m: M, _ n: N, _ o: O, _ p: P, _ q: Q, _ s: S, _ t: T, _ u: U, _ v: V, _ w: W, _ x: X, _ y: Y, _ z: Z, of f: @differentiable(reverse) (L, M, N, O, P, Q, S, T, U, V, W, X, Y, Z) -> R
+        ) -> (value: R,
+              pullback: (R.TangentVector)
+                -> (L.TangentVector, M.TangentVector, N.TangentVector, O.TangentVector, P.TangentVector, Q.TangentVector, S.TangentVector, T.TangentVector, U.TangentVector, V.TangentVector, W.TangentVector, X.TangentVector, Y.TangentVector, Z.TangentVector)) {
+
+            let (value, pullback) = valueWithPullback(at: Pair(Pair(Pair(l, m), Pair(n, o)), Pair(p, q)), Pair(Pair(s, t), Pair(u, v)), Pair(Pair(w, x), Pair(y, z))) { pair1, pair2, pair3 in 
+                f(pair1.a.a.a, pair1.a.a.b, pair1.a.b.a, pair1.a.b.b, pair1.b.a, pair1.b.b, pair2.a.a, pair2.a.b, pair2.b.a, pair2.b.b, pair3.a.a, pair3.a.b, pair3.b.a, pair3.b.b)
+            }
+
+            return (
+                value: value,
+                pullback: { v in 
+                    let results = pullback(v)
+                    return (results.0.a.a.a, results.0.a.a.b, results.0.a.b.a, results.0.a.b.b, results.0.b.a, results.0.b.b, results.1.a.a, results.1.a.b, results.1.b.a, results.1.b.b, results.2.a.a, results.2.a.b, results.2.b.a, results.2.b.b)
+                }
+            )
+        }
+        
         #endif
 
         """
