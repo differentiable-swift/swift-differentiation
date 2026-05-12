1642fe25d69142a22865c62de5115f9b4a83241a
Jaap Wijnen <wijnen.jaap@gmail.com>
2026-05-12 19:18:56 +0200

    fix: fix insert vjp bug

--- Sources/Differentiation/Array+Insert.swift
+++ Sources/Differentiation/Array+Insert.swift
@@ -3,8 +3,12 @@ import _Differentiation
 extension Array where Element: Differentiable {
     @derivative(of: insert)
     @inlinable
-    public mutating func _vjpInsert(_: Element, at i: Int) -> (value: Void, pullback: (inout TangentVector) -> Element.TangentVector) {
-        (
+    public mutating func _vjpInsert(
+        _ newElement: Element,
+        at i: Int
+    ) -> (value: Void, pullback: (inout TangentVector) -> Element.TangentVector) {
+        self.insert(newElement, at: i)
+        return (
             value: (),
             pullback: { v in
                 v.remove(at: i)
--- Tests/DifferentiationTests/Array+InsertTests.swift
+++ Tests/DifferentiationTests/Array+InsertTests.swift
@@ -3,16 +3,32 @@ import Testing
 
 struct ArrayInsertTests {
     @Test func insert() {
-        var array: [Float] = [1.0, 2.0, 3.0]
-        array.insert(4.0, at: 0)
-        #expect(array.count == 4)
-        #expect(array[0] == 4)
+        var array1: [Float] = [1.0, 2.0, 3.0]
+        array1.insert(4.0, at: 0)
+        #expect(array1.count == 4)
+        #expect(array1[0] == 4)
 
         // Test differentiation.
-        let (_, insertPullback) = array._vjpInsert(4.0, at: 0)
+        var array2: [Float] = [1.0, 2.0, 3.0]
+        let (_, insertPullback) = array2._vjpInsert(4.0, at: 0)
+        #expect(array2 == [4.0, 1.0, 2.0, 3.0])
         var insertTangent = Array<Float>.TangentVector([4.0, 3.0, 2.0])
         let insertValue = insertPullback(&insertTangent)
         #expect(insertValue == 4.0)
         #expect(insertTangent.count == 2)
+
+        let array3: [Float] = [1.0, 2.0, 3.0]
+        let value: Float = 4.0
+        let vwpb = valueWithPullback(at: array3, value, of: { array, value in
+            var array = array
+            array.insert(value, at: 0)
+            return array
+        })
+
+        #expect(vwpb.value == [4.0, 1.0, 2.0, 3.0])
+        let tangent: [Float].TangentVector = [4.0, 3.0, 2.0]
+        let gradient = vwpb.pullback(tangent)
+        #expect(gradient.0 == [3.0, 2.0])
+        #expect(gradient.1 == 4.0)
     }
 }
